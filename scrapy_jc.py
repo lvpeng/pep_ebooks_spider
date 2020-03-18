8b9d1a66248cf12e923a3fd5fe09d63bf344d81a
lvpeng <lvpeng-333@163.com>
2020-03-18 22:03:51 +0800

    update

--- scrapy_jc.py
+++ scrapy_jc.py
@@ -13,6 +13,8 @@ url = 'https://bp.pep.com.cn/jc/'
 res = requests.get(url)
 res.encoding = 'utf-8'
 req_url = res.url
+books_counts=0
+books_downloaded_count=0
 ## response:
 ##   content-type: text/html
 
@@ -46,15 +48,19 @@ req_url = res.url
 data = {}
 data['books']=[]
 
-def getEbooksRequest(url, book_dir):
+def getEbooksRequest(url, book_dir, grade_stage, subject_name):
+    global books_counts
     ebooks_url = os.path.join(req_url, url )
+    print('Request {0}/{1} book'.format(grade_stage, subject_name))
     response= requests.get(ebooks_url)
     response.encoding='utf-8'
     if response.status_code == 200:
         soup = BeautifulSoup(response.text, 'lxml')
+        #print('Success & Close Connection')
+        response.close()   
         book_nodes = soup.find_all("li", class_="fl js_cp")
-        books_counts = len(book_nodes)
-        #print(books_counts)
+        
+        books_counts += len(book_nodes)
         # book name
         for _book in book_nodes:
             book = {}
@@ -66,7 +72,7 @@ def getEbooksRequest(url, book_dir):
             book['url'] = _book_dow_fullurl
             book['down_dir_path'] =  book_dir
             data['books'].append(book);
-    response.close()   
+    
        
 def doWork():
     while True:
@@ -75,15 +81,32 @@ def doWork():
         #doSomethingWithResult(_status, _book['url'])
         
         q.task_done()
+        print('Downloaded End , TOTAL BOOKS COUNTS: {0}'.format(books_downloaded_count))
+        sys.exit(1)
 
 def downloadEbooks(_book):
     try:
+        global books_downloaded_count
         _book_abs_path = os.path.join(_book['down_dir_path'], _book['name']+'.pdf')
-        with open(_book_abs_path, 'wb') as f:
-            resp = requests.get(_book['url'])
-            f.write(resp.content)
-            return resp.status
-        resp.close()
+        _book_grade = _book['down_dir_path'].split('\\')[-2]
+        _book_subject = _book['down_dir_path'].split('\\')[-1]
+        _book_name = _book['name']
+        print('{0}/{1}/{2} Downloading....'.format(_book_name, _book_grade , _book_subject))
+        resp = requests.get(_book['url'])
+        if resp.status_code == 200:
+            
+            with open(_book_abs_path, 'wb') as f:
+                ##for chunk in res.iter_content(chunk_size=128):
+                ##    f.write(chunk)
+                ## return resp.status
+                try: 
+                    f.write(resp.content)
+                    books_downloaded_count+=1
+                    print('{0}/{1}/{2} Downloaded Successfully!'.format(_book_name, _book_grade , _book_subject))
+                except err:
+                    print(err)
+                    sys.exit(1)
+            resp.close()
     except:
         return "error: "
 
@@ -107,6 +130,7 @@ if res.status_code == 200:
         _container['dir'] = item.find('h5').string
         # 1. create parent dir
         Path(os.path.join(pep_dir,_container['dir'])).mkdir(parents=True, exist_ok=True)
+        #print('{0} Dir Created'.format(_container['dir']))
         _container['subject'] = []
         list_grade_links = item.select('ul.clearfix.js_cp > li.fl > a')
         for link in list_grade_links:
@@ -114,7 +138,7 @@ if res.status_code == 200:
             subject['subdir'] = link.get_text()
             #2 create subject dir
             Path(os.path.join(pep_dir,_container['dir'], subject['subdir'] )).mkdir(parents=True, exist_ok=True)
-            
+            #print('{0} Dir Created'.format(subject['subdir']))
             ## request detail url     
             subject['link'] = link['href']
             # book's dir path to download in 
@@ -127,17 +151,19 @@ if res.status_code == 200:
             '''
             #print(_book_dir)
             #3 request to get sub dir books 
-            getEbooksRequest(link['href'], _book_dir)
+            getEbooksRequest(link['href'], _book_dir, _container['dir'], subject['subdir'])
 
      # 4 write data['books'] to json file
         
     with open('./data.json','w') as outfile:
         json.dump(data,outfile,ensure_ascii=False,indent=4)
     
+    print('###Total BOOKS: {0}'.format(books_counts))
     # 5  Queue 
     concurrent = 2000
     q = queue.Queue(concurrent * 2)
     for i in range(concurrent):
+        
         t = Thread(target=doWork)
         t.daemon = True
         t.start()
